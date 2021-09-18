66dc0b24f8f12ab156bc5ceaa324029b186a3efa
neilson <jwilson@users.noreply.github.com>
2021-09-18 00:34:09 -0400

    tweak

--- day1a.ex
+++ day1a.ex
@@ -6,9 +6,9 @@ split_contents = contents \
 defmodule Advent1 do
   def calc([head | tail]) do
     compared_value = Enum.reduce_while(tail, 0, fn x, _ ->
-      if x + head == 2020, do: {:halt, x * -1}, else: {:cont, x}
+      if x + head == 2020, do: {:halt, -x}, else: {:cont, x}
     end)
-    if compared_value > 0, do: Advent1.calc(tail), else: head * compared_value * -1
+    if compared_value > 0, do: Advent1.calc(tail), else: head * -compared_value
   end
 end
 
